d6897b80f8d22d6607c91643398b6be9944cde3c
Saurabh9999 <saurabhsahu7247@gmail.com>
2026-07-22 00:11:33 +0530

    change in UI

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  // baseURL: "https://real-time-chat-app-vcad.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://real-time-chat-app-vcad.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
